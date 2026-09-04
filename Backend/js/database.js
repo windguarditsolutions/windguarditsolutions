562c22388581756d285ed532db4e72b8aeeeb0ad
windguarditsolutions <windguarditsolutions@gmail.com>
2026-09-04 20:24:23 +0530

    Update database.js

--- Backend/js/database.js
+++ Backend/js/database.js
@@ -203,7 +203,7 @@ const webDatabase = {
                 bg:
                     "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9iaWxlJTIwYXBwJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D",
                 icon:
-                    "https://www.nicepng.com/png/full/259-2592669_ios-android-icon-png-clip-art-free-ios.png",
+                    "https://www.pngkey.com/png/detail/129-1290553_choose-one-of-our-pro-kits-today-to.png",
                 title:
                     "Mobile App Development",
                 desc:
