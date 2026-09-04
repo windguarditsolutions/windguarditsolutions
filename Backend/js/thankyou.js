8804d294cdfa283a348fa77d932a9f4df8a1b0e2
windguarditsolutions <windguarditsolutions@gmail.com>
2026-09-04 14:04:30 +0530

    Update thankyou.js

--- Backend/js/thankyou.js
+++ Backend/js/thankyou.js
@@ -1,34 +1,21 @@
 function toggleMenu() {
     const nav = document.getElementById("navLinks");
-
     if (nav) {
         nav.classList.toggle("active");
     }
 }
-
 document.addEventListener("DOMContentLoaded", function () {
-
     const countdownEl = document.getElementById("countdown");
-
     if (!countdownEl) return;
-
     let timeLeft = 5;
-
     const timer = setInterval(() => {
-
         timeLeft--;
-
         countdownEl.textContent = timeLeft;
-
         if (timeLeft <= 0) {
-
             clearInterval(timer);
-
-            // Frontend homepage
             window.location.replace(
-                "http://localhost:5500/Frontend/index.html"
+                "https://windguarditsolution.onrender.com"
             );
         }
-
     }, 1000);
 });
